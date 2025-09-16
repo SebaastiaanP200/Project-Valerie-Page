edcedbca2508f260f024bdf83e00efd1d7445669
Sebastián Pérez <sebastiaan.peerez@gmail.com>
2025-09-16 19:45:58 -0300

    V.19.45

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,41 @@
-/* PORTFOLIO (FOR) */
+/* PORTFOLIO */
 
+const containerOpNf = document.querySelector(".grid__container");
 
+const getOpNf = async () => {
+    try {
+        const res = await axios.get("../img.json");
+        const data = res.data;
+        const oPfragment = document.createDocumentFragment();
+        
+        for (let i = 0; i < data.length; i++) {
+            const portfolioObject = Object.values(data[i])[0];
+            
+            if (!portfolioObject || !portfolioObject.link || !Array.isArray(portfolioObject.images)) {continue;}
+            
+            const link = portfolioObject.link;
+            const images = portfolioObject.images;
+            
+            const ulContainer = images.map((imageObject) => {
+                const alt = imageObject.name;
+                const src = imageObject.url;
+                const id = imageObject.id;
+                
+                return `<li><img src="${src}" alt="${alt}" id="${id}"></li>`
+            }).join("");
+            
+            const div = document.createElement("DIV");
+            div.classList.add("slider-box");
+            div.innerHTML = 
+                            `<a href="${link}" class="link__portfolio">Open Portfolio</a>
+                            <ul>${ulContainer}</ul>`;
+            oPfragment.appendChild(div);
+        }
+        containerOpNf.appendChild(oPfragment);
+    } catch (e) {console.error("Error durante la carga: ", e);}
+};
 
-
+getOpNf();
 
 
 
